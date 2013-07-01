3bfec4e4e4d07de9077dd58176efb8e610581933
Ryoto <ruder.com@gmail.com>
2013-07-01 23:05:09 +0900

    Initial Commit

---
+++ AMSystem/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AMSystem
+//
+//  Created by Maeda Ryoto on 13/07/01.
+//  Copyright (c) 2013年 abcc_joko4. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
