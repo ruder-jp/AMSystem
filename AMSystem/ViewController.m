efd5a25515a9965b3fbbff7d6e96b5db1d46412f
Ryoto <ruder.com@gmail.com>
2013-07-05 13:06:26 +0900

    西尾

---
+++ AMSystem/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AMSystem
+//
+//  Created by Maeda Ryoto on 13/07/01.
+//  Copyright (c) 2013年 abcc_joko4. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ AMSystemTests/AMSystemTests.h
@@ -0,0 +1,13 @@
+//
+//  AMSystemTests.h
+//  AMSystemTests
+//
+//  Created by Maeda Ryoto on 13/07/01.
+//  Copyright (c) 2013年 abcc_joko4. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface AMSystemTests : SenTestCase
+
+@end
